f1a8c03a1c3cd108ac23f8829af3208d09ed34bd
Charles Kincy <cpk1971@gmail.com>
2024-06-16 22:15:01 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Charles Kincy on 2024-06-16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
