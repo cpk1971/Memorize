3b8b41d160bbe4024723806ce051264d03df7165
Charles Kincy <cpk1971@gmail.com>
2024-07-04 15:39:10 -0700

    refactor to AspectVGrid

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -23,39 +23,14 @@ struct EmojiMemoryGameView: View {
     }
     
     var cards: some View {
-        GeometryReader { geometry in
-            let gridItemSize = gridItemWidthThatFits(
-                count: viewModel.cards.count,
-                size: geometry.size,
-                atAspectRatio: cardAspectRatio)
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-                ForEach(viewModel.cards) { card in
-                    CardView(card)
-                        .aspectRatio(cardAspectRatio, contentMode: .fit)
-                        .padding(4)
-                        .onTapGesture {
-                            viewModel.choose(card)
-                        }
+        AspectVGrid(items: viewModel.cards, aspectRatio: cardAspectRatio) { card in
+            CardView(card)
+                .aspectRatio(cardAspectRatio, contentMode: .fit)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
                 }
-            }.foregroundColor(.orange)
-        }
-    }
-    
-    func gridItemWidthThatFits(count: Int, size: CGSize, atAspectRatio aspectRatio: CGFloat) -> CGFloat {
-        print(size)
-        let count = CGFloat(count)
-        var columnCount = 1.0
-        repeat {
-            let width = size.width / columnCount
-            let height = width / aspectRatio
-            let rowCount = (count / columnCount).rounded(.up)
-            if rowCount * height < size.height {
-                return (size.width / columnCount).rounded(.down)
-            }
-            columnCount += 1.0
-        } while columnCount < count
-        
-        return min(size.width / count, (size.height * aspectRatio).rounded(.down))
+        }.foregroundColor(.orange)
     }
     
     init(viewModel: EmojiMemoryGame) {
