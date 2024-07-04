eb5a630eef787fb446a108b08c1e7d30af4b71a7
Charles Kincy <cpk1971@gmail.com>
2024-07-04 16:10:11 -0700

    More refactoring

---
+++ Memorize/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Charles Kincy on 2024-07-04.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
