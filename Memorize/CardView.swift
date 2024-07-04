c00b0673dee0e1e897d9f363d4952b907869db47
Charles Kincy <cpk1971@gmail.com>
2024-07-04 16:18:40 -0700

    tweak

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -16,17 +16,6 @@ struct CardView: View {
         self.card = card
     }
     
-    private struct Constants {
-        static let cornerRadius: CGFloat = 12
-        static let lineWidth: CGFloat = 2
-        static let inset: CGFloat = 5
-        struct FontSize {
-            static let largest: CGFloat = 200
-            static let smallest: CGFloat = 10
-            static let scaleFactor = smallest / largest
-        }
-    }
-    
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
@@ -42,6 +31,17 @@ struct CardView: View {
             .opacity(card.isFaceUp ? 1 : 0)
             base.fill().opacity(card.isFaceUp ? 0 : 1)
         }.opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+}
+
+private struct Constants {
+    static let cornerRadius: CGFloat = 12
+    static let lineWidth: CGFloat = 2
+    static let inset: CGFloat = 5
+    struct FontSize {
+        static let largest: CGFloat = 200
+        static let smallest: CGFloat = 10
+        static let scaleFactor = smallest / largest
     }
 }
 
