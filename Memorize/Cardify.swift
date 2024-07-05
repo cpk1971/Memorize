f4de89b85aaf0e3c1df0f7b2172e70ffdde88512
Charles Kincy <cpk1971@gmail.com>
2024-07-04 20:01:36 -0700

    add explicit animations

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,8 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Charles Kincy on 2024-07-04.
+//
+
+import Foundation
---
+++ Memorize/Pie.swift
@@ -0,0 +1,8 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Charles Kincy on 2024-07-04.
+//
+
+import Foundation
