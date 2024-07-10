d14b4bf452f6ef30258320629954fa63569ec5dc
Charles Kincy <cpk1971@gmail.com>
2024-07-10 16:33:42 -0700

    some more animations

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
+    typealias Card = MemoryGame<String>.Card
+    
     @ObservedObject var viewModel: EmojiMemoryGame
     private let cardAspectRatio: CGFloat = 4/5
     
@@ -42,16 +44,27 @@ struct EmojiMemoryGameView: View {
     private var cards: some View {
         AspectVGrid(viewModel.cards, aspectRatio: cardAspectRatio) { card in
             CardView(card)
-                .aspectRatio(cardAspectRatio, contentMode: .fit)
                 .padding(4)
+                .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
+                .zIndex(scoreChange(causedBy: card) != 0 ? 1000 : 0)
                 .onTapGesture {
                     withAnimation(.easeInOut(duration: 0.5)) {
+                        let scoreBeforeChoosing = viewModel.score
                         viewModel.choose(card)
+                        let scoreChange = viewModel.score - scoreBeforeChoosing
+                        lastScoreChange = (scoreChange, card.id)
                     }
                 }
         }
     }
     
+    @State private var lastScoreChange: (Int, causedByCardId: Card.ID)
+        = (0, causedByCardId: UUID())
+    
+    private func scoreChange(causedBy card: Card) -> Int {
+        let (amount, id) = lastScoreChange
+        return card.id == id ? amount : 0
+    }
     
     init(viewModel: EmojiMemoryGame) {
         self.viewModel = viewModel
---
+++ Memorize/FlyingNumber.swift
@@ -0,0 +1,45 @@
+//
+//  FlyingNumber.swift
+//  Memorize
+//
+//  Created by Charles Kincy on 2024-07-04.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    let number: Int
+    
+    @State private var offset: CGFloat = 0
+    
+    var body: some View {
+        if number != 0 {
+            Text(number, format: .number.sign(strategy: .always()))
+                .font(.largeTitle)
+                .foregroundColor(number < 0 ? .red : .green)
+                .zIndex(1000)
+                .shadow(color: .black, radius: 1.5, x: 1, y: 1)
+                .offset(x: 0, y: offset)
+                .opacity(offset != 0 ? 0 : 1)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.5)) {
+                        offset = number < 0 ? 200 : -200
+                    }
+                }
+                .onDisappear {
+                    offset = 0
+                }
+        }
+        
+    }
+}
+
+#Preview {
+    VStack {
+        FlyingNumber(number: 5)
+        Divider()
+        FlyingNumber(number: 0)
+        Divider()
+        FlyingNumber(number: -2)
+    }
+}
